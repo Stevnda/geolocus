d79b2708d6da5573ed1a476bba91d942b31b895a
kxh4892636 <kxh4892636@gmail.com>
2024-12-19 19:31:57 +0800

    refactor: merge some temp props of geolocusObject

--- package/core/src/object/object.ts
+++ package/core/src/object/object.ts
@@ -2,11 +2,10 @@ import { generateUUID, GeolocusGrid, Grid } from '@/util'
 import jsts from '@geolocus/jsts'
 import { GeolocusGeometry, JTSGeometryFactory } from './geometry'
 
-interface GeolocusObjectProps {
+interface GeolocusObjectInit {
   name?: string | null // 默认值 null
   templateName?: string | null // 默认值 null
   status?: 'fuzzy' | 'precise' // 默认值 precise
-  infinity?: boolean // 默认值 false
 }
 
 export class GeolocusObject {
@@ -15,20 +14,18 @@ export class GeolocusObject {
   private _templateName: string | null
   private _status: 'fuzzy' | 'precise'
   private _geometry: GeolocusGeometry
-  private _infinity: boolean // 用于判断该对象是否通过无线距离生成
   private _props: Record<string, unknown>
 
   constructor(
     geometry: GeolocusGeometry,
-    init?: GeolocusObjectProps,
-    props?: Record<string, unknown>,
+    init?: GeolocusObjectInit, // useful props
+    props?: Record<string, unknown>, // temp props
   ) {
     this._uuid = generateUUID()
     this._geometry = geometry
     this._name = init?.name || null
     this._templateName = init?.templateName || null
     this._status = init?.status || 'precise'
-    this._infinity = init?.infinity || false
     this._props = props || {}
   }
 
@@ -72,20 +69,20 @@ export class GeolocusObject {
     return this._geometry
   }
 
-  getInfinity(): boolean {
-    return this._infinity
-  }
-
-  setInfinity(value: boolean): void {
-    this._infinity = value
-  }
-
   getProps(): Record<string, unknown> {
     return this._props
   }
 
   setProps(value: Record<string, unknown>): void {
     this._props = value
+  }
+
+  addProp(key: string, value: unknown): void {
+    this._props[key] = value
+  }
+
+  getProp(key: string): unknown {
+    return this._props[key]
   }
 }
 
